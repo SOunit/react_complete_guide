8118b529ca4dde1b4dd9dd697e2c05b9150f050f
SOunit <workbookwork7@gmail.com>
2021-06-13 22:48:27 +0900

    add state to functional component

--- 04_state_events/src/components/Expenses/ExpenseItem.js
+++ 04_state_events/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,24 @@
+import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
+// if state change, react re-execute this function in which useState exists
 const ExpanseItem = (props) => {
+  // ask react to control value
+  // this initialization do NOT run twice becuase react remember first time execution.
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log('test');
+    // set value, call this function, re-render
+    setTitle('Updated!');
   };
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Click</button>
