fc46c4154c636333bc2a685fe374de788b30bb08
SOunit <workbookwork7@gmail.com>
2021-06-13 15:14:20 +0900

    add style to component

--- 03_react_basic/src/components/ExpenseItem.js
+++ 03_react_basic/src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpanseItem() {
   return (
-    <div>
+    <div className='expense-item'>
       <div>March 28th 2021</div>
-      <div>
+      <div className='expense-item__description'>
         <h2>Car Insurance</h2>
-        <div>$247.76</div>
+        <div className='expense-item__price'>$247.76</div>
       </div>
     </div>
   );
