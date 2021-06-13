59183a7f75292219214944a19b50760bbc252e56
SOunit <workbookwork7@gmail.com>
2021-06-13 22:17:26 +0900

    add button and click event

--- 04_state_events/src/components/Expenses/ExpenseItem.js
+++ 04_state_events/src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,10 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpanseItem = (props) => {
+  const clickHandler = () => {
+    console.log('test');
+  };
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -10,6 +14,7 @@ const ExpanseItem = (props) => {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Click</button>
     </Card>
   );
 };
