b775c88d4b066316993a0c55e304d2ffa70dc629
SOunit <workbookwork7@gmail.com>
2021-06-13 16:29:06 +0900

    add expense list

--- 03_react_basic/src/App.js
+++ 03_react_basic/src/App.js
@@ -1,10 +1,42 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id: '1',
+      title: 'Car Insurance',
+      amount: 24.33,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: '2',
+      title: 'Book Insurance',
+      amount: 24.33,
+      date: new Date(2021, 3, 28),
+    },
+    {
+      id: '3',
+      title: 'House Insurance',
+      amount: 24.33,
+      date: new Date(2021, 4, 28),
+    },
+  ];
+
+  const list = expenses.map((expense) => {
+    return (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    );
+  });
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      {list}
     </div>
   );
 }
