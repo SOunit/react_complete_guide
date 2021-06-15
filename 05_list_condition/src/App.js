376aa68f0a31ef36ca1c67fbc9bffc5bd5a95d42
SOunit <workbookwork7@gmail.com>
2021-06-16 08:09:17 +0900

    use stateful list

--- 05_list_condition/src/App.js
+++ 05_list_condition/src/App.js
@@ -1,36 +1,47 @@
+import { useState } from 'react';
+
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+  {
+    id: '1',
+    title: 'Car Insurance',
+    amount: 24.33,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: '2',
+    title: 'Book Insurance',
+    amount: 24.33,
+    date: new Date(2021, 3, 28),
+  },
+  {
+    id: '3',
+    title: 'House Insurance',
+    amount: 24.33,
+    date: new Date(2021, 4, 28),
+  },
+  {
+    id: '4',
+    title: 'House Insurance',
+    amount: 44.33,
+    date: new Date(2021, 4, 28),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: '1',
-      title: 'Car Insurance',
-      amount: 24.33,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: '2',
-      title: 'Book Insurance',
-      amount: 24.33,
-      date: new Date(2021, 3, 28),
-    },
-    {
-      id: '3',
-      title: 'House Insurance',
-      amount: 24.33,
-      date: new Date(2021, 4, 28),
-    },
-    {
-      id: '4',
-      title: 'House Insurance',
-      amount: 44.33,
-      date: new Date(2021, 4, 28),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log(expense);
+    // this is NOT collect!!!!
+    // setExpenses([...expenses, expense]);
+
+    // function pattern is collect
+    setExpenses((prevExpenses) => {
+      console.log(prevExpenses);
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
