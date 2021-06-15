7e673bcd5075450dffe4e4561825a3fc7f09527c
SOunit <workbookwork7@gmail.com>
2021-06-15 20:13:00 +0900

    add filter year

--- 04_01_state_events/src/components/Expenses/Expenses.js
+++ 04_01_state_events/src/components/Expenses/Expenses.js
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+
 import './Expenses.css';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   const items = props.items.map((item) => {
     return (
       <ExpenseItem
@@ -14,7 +24,17 @@ const Expenses = (props) => {
     );
   });
 
-  return <Card className='expenses'>{items}</Card>;
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {items}
+      </Card>
+    </div>
+  );
 };
 
 export default Expenses;
