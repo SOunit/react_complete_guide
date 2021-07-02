f0f83c455634419430056a21cef595a4d22a5c1d
SOunit <workbookwork7@gmail.com>
2021-07-03 08:19:59 +0900

    add custom button component

--- 08_02_from_lecture/src/components/Users/AddUser.js
+++ 08_02_from_lecture/src/components/Users/AddUser.js
@@ -1,19 +1,44 @@
+import { useState } from 'react';
+
 import Card from '../UI/Card';
+import Button from '../UI/Button';
+
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
+  const [enteredUserName, setEnteredUserName] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
   const addUserHandler = (event) => {
     event.preventDefault();
+  };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text' />
+        <input
+          id='username'
+          type='text'
+          value={enteredUserName}
+          onChange={userNameChangeHandler}
+        />
         <label htmlFor='age'>Ages (Years)</label>
-        <input id='age' type='number' />
-        <button type='submit'>Add User</button>
+        <input
+          id='age'
+          type='number'
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
+        <Button type='submit'>Add User</Button>
       </form>
     </Card>
   );
