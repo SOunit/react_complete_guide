b6d54c3599836ee09a8971326392c68f21df8849
SOunit <workbookwork7@gmail.com>
2021-07-04 08:33:02 +0900

    add error state

--- 08_02_from_lecture/src/components/Users/AddUser.js
+++ 08_02_from_lecture/src/components/Users/AddUser.js
@@ -9,6 +9,7 @@ import classes from './AddUser.module.css';
 const AddUser = (props) => {
   const [enteredUserName, setEnteredUserName] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
 
   const userNameChangeHandler = (event) => {
     setEnteredUserName(event.target.value);
@@ -23,9 +24,17 @@ const AddUser = (props) => {
 
     // validation
     if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values).',
+      });
       return;
     }
     if (+enteredAge < 0) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0).',
+      });
       return;
     }
 
@@ -36,9 +45,19 @@ const AddUser = (props) => {
     setEnteredAge('');
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      <ErrorModal title='title' message='message' />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor='username'>Username</label>
