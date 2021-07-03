778dfe23fc1a13e41a77e22f3f02b3bce8bf5615
SOunit <workbookwork7@gmail.com>
2021-07-03 09:04:20 +0900

    add users list component

--- 08_02_from_lecture/src/App.js
+++ 08_02_from_lecture/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const DUMMY_DATA = [
+    { id: 1, name: 'test', age: 10 },
+    { id: 2, name: 'test', age: 10 },
+  ];
+
   return (
     <div>
       <AddUser />
+      <UsersList users={DUMMY_DATA} />
     </div>
   );
 }
