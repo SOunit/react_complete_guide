14a436fcbcb44c71bd6cf7146e4549530a804d47
SOunit <workbookwork7@gmail.com>
2021-07-08 12:37:32 +0900

    add cart-modal state

--- 11_02_effetcs_reducers_contexts/src/components/Layout/Header.js
+++ 11_02_effetcs_reducers_contexts/src/components/Layout/Header.js
@@ -5,12 +5,12 @@ import HeaderCartButton from './HeaderCartButton';
 import classes from './Header.module.css';
 import mealsImage from '../../assets/meals.jpg';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton>Cart</HeaderCartButton>
+        <HeaderCartButton onClick={props.onShowCart}>Cart</HeaderCartButton>
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='table full of delicious food!' />
