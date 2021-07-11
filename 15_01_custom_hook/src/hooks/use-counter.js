3dbbeae94ea4d31f376289b7c35d06887de14a3b
SOunit <workbookwork7@gmail.com>
2021-07-11 21:05:46 +0900

    add custom hooks to backward-counter component

--- 15_01_custom_hook/src/hooks/use-counter.js
+++ 15_01_custom_hook/src/hooks/use-counter.js
@@ -1,17 +1,21 @@
 import { useState, useEffect } from 'react';
 
 // custom hooks, naming is the most important setting
-const useCounter = () => {
+const useCounter = (forwards = true) => {
   // you can use other hooks in custom hooks
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]);
 
   return counter;
 };
