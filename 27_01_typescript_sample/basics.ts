8e7b7a0c473bce340d3f11250fde944c6fd47cc0
SOunit <workbookwork7@gmail.com>
2021-07-12 15:56:05 +0900

    add primitive types of typescript

---
+++ 27_01_typescript_sample/basics.ts
@@ -0,0 +1,15 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parametars
+
+// Primitives
+let age: number = 15;
+age = 10;
+age = 12.1;
+// age = '123';
+
+let userName: string;
+userName = 'Jack';
+
+let isInstructor: boolean;
+isInstructor = true;
