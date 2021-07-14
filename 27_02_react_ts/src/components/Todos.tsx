a21e0148d8cff7e22b4ac5e974c4fbc028d641bc
SOunit <workbookwork7@gmail.com>
2021-07-14 23:36:45 +0900

    add todos with typescript

--- 27_02_react_ts/src/components/Todos.tsx
+++ 27_02_react_ts/src/components/Todos.tsx
@@ -1,10 +1,15 @@
-function Todos() {
+import React from 'react';
+
+// FC = Functional Component
+// this type offers base setup for Functional component in typescript
+const Todos: React.FC<{ items: string[] }> = (props) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn TypeScript</li>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
