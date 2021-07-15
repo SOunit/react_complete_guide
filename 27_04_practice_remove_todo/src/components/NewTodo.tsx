bb361addac9a0e4a309aae404b3b58bcc8dcc1ed
SOunit <workbookwork7@gmail.com>
2021-07-14 17:45:38 -0700

    add new project for practice

---
+++ 27_04_practice_remove_todo/src/components/NewTodo.tsx
@@ -0,0 +1,35 @@
+import { useRef } from 'react';
+
+import classes from './NewTodo.module.css';
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  // this is useRef initial setup
+  // 1. ref type is html-input-element in this case. other sample is HTMLButtonElement.
+  // 2. initial value is null, connected with input element in return statement
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    // typescript put ? mark here, because of possibility of nullish value
+    // but developer can put ! mark, to tell typescript value is not null
+    // this here, connection is done so never be null
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      <label htmlFor='text'>Todo text</label>
+      <input type='text' id='text' ref={todoTextInputRef} />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
