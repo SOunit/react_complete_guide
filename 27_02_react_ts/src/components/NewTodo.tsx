8df0d51abb72cb7c27ab0534b719281f2110f601
SOunit <workbookwork7@gmail.com>
2021-07-14 17:43:27 -0700

    add styling

--- 27_02_react_ts/src/components/NewTodo.tsx
+++ 27_02_react_ts/src/components/NewTodo.tsx
@@ -1,5 +1,7 @@
 import { useRef } from 'react';
 
+import classes from './NewTodo.module.css';
+
 const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   // this is useRef initial setup
   // 1. ref type is html-input-element in this case. other sample is HTMLButtonElement.
@@ -22,7 +24,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <label htmlFor='text'>Todo text</label>
       <input type='text' id='text' ref={todoTextInputRef} />
       <button>Add Todo</button>
--- 27_02_react_ts/src/components/Todos.tsx
+++ 27_02_react_ts/src/components/Todos.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Todo from '../models/todo';
 import TodoItem from '../components/TodoItem';
 
+import classes from './Todos.module.css';
+
 // FC = Functional Component
 // this type offers base setup for Functional component in typescript
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => (
         <TodoItem text={item.text} key={item.id} />
       ))}
