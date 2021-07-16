694baee8dc517aa524af4a6af7e1e3cdfbd2ec7b
SOunit <workbookwork7@gmail.com>
2021-07-16 06:51:51 -0700

    add validator for first-name

--- 16_05_practice_form_validation/src/components/BasicForm.js
+++ 16_05_practice_form_validation/src/components/BasicForm.js
@@ -1,10 +1,52 @@
+import { useState } from 'react';
+
+// behavior
+// 1. instant error check
+// 2. instant error recovery
+// 3. show error message only after touch
+
+// todo to get behavior
+// 1. show error message
+// 2. check if value is invalid
+// 3. check if touched
+// 4. check if hasError(=invalid && touched)
+// 5. add check for blur
+// 6. add check for key touch to recover error
+// 7. add error style to input form
 const BasicForm = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  let enteredNameIsValid = false;
+  if (enteredName.trim() !== '') {
+    enteredNameIsValid = true;
+  }
+
+  let hasError = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const nameInputBlurHandler = () => {
+    setEnteredNameTouched(true);
+  };
+
+  const nameInputClasses = hasError ? 'form-control invalid' : 'form-control';
+
   return (
     <form>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={nameInputClasses}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input
+            type='text'
+            id='name'
+            value={enteredName}
+            onChange={nameInputChangeHandler}
+            onBlur={nameInputBlurHandler}
+          />
+          {hasError && <p className='error-text'>Name is empty.</p>}
         </div>
         <div className='form-control'>
           <label htmlFor='name'>Last Name</label>
