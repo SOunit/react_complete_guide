992b55eef19410eafd46d7aec44d2486caa5d93d
SOunit <workbookwork7@gmail.com>
2021-08-13 07:30:45 -0700

    add dynamic route

--- 20_01_spa/src/pages/Products.js
+++ 20_01_spa/src/pages/Products.js
@@ -1,5 +1,14 @@
 const Products = () => {
-  return <h1>Products</h1>;
+  return (
+    <section>
+      <h1>Products</h1>
+      <ul>
+        <li>Product1</li>
+        <li>Product2</li>
+        <li>Product3</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
