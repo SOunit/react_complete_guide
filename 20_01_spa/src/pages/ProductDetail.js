cac8f98a96ec1880f39905ca61428f6eed269305
SOunit <workbookwork7@gmail.com>
2021-08-13 17:25:34 -0700

    add params to product detail page

--- 20_01_spa/src/pages/ProductDetail.js
+++ 20_01_spa/src/pages/ProductDetail.js
@@ -1,5 +1,16 @@
+import { useParams } from 'react-router-dom';
+
 const ProductDetail = () => {
-  return <h1>Product Detail</h1>;
+  const params = useParams();
+
+  console.log(params.productId);
+
+  return (
+    <section>
+      <h1>Product Detail</h1>
+      <p>{params.productId}</p>
+    </section>
+  );
 };
 
 export default ProductDetail;
