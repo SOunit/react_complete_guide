36d4c28c06ff0f8b9cd4cfe82fe2e5d5524e4bc8
SOunit <workbookwork7@gmail.com>
2021-08-13 17:46:20 -0700

    add switch, exact for react routes work correct

--- 20_01_spa/src/pages/Products.js
+++ 20_01_spa/src/pages/Products.js
@@ -1,11 +1,19 @@
+import { Link } from 'react-router-dom';
+
 const Products = () => {
   return (
     <section>
       <h1>Products</h1>
       <ul>
-        <li>Product1</li>
-        <li>Product2</li>
-        <li>Product3</li>
+        <li>
+          <Link to='/products/p1'>Product1</Link>
+        </li>
+        <li>
+          <Link to='/products/p2'>Product2</Link>
+        </li>
+        <li>
+          <Link to='/products/p3'>Product3</Link>
+        </li>
       </ul>
     </section>
   );
