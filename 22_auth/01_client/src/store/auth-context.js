4b1f55a1d703da01bb1b21f82087779ebcad10b5
SOunit <workbookwork7@gmail.com>
2021-09-06 07:11:15 -0700

    add token to local strage

--- 22_auth/01_client/src/store/auth-context.js
+++ 22_auth/01_client/src/store/auth-context.js
@@ -8,17 +8,20 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem('token');
+  const [token, setToken] = useState(initialToken);
 
   // return true if token is truecy, if its string
   const userIsLoggedIn = !!token;
 
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem('token', token);
   };
 
   const logoutHanlder = () => {
     setToken(null);
+    localStorage.removeItem('token');
   };
 
   const contextValue = {
