aaf65fbc22e09215433a840cf7f3cd252797a815
SOunit <workbookwork7@gmail.com>
2021-10-09 16:42:45 -0700

    add fetching query value sample

--- 23_nextjs/01_nextjs-course/pages/news/[newsId].js
+++ 23_nextjs/01_nextjs-course/pages/news/[newsId].js
@@ -1,6 +1,11 @@
 // our-domain.com/news/something-important
+import { useRouter } from 'next/router';
 
 function DetailPage() {
+  const router = useRouter();
+
+  console.log(router.query.newsId);
+
   return <h1>DetailPage</h1>;
 }
 
