1eb0a6e72999c776956982e3f52dc79a3acb5c9e
SOunit <workbookwork7@gmail.com>
2021-11-06 04:37:20 -0700

    add payload

--- 18_redux_intro/02_redux_react/src/store/index.js
+++ 18_redux_intro/02_redux_react/src/store/index.js
@@ -4,6 +4,8 @@ const counterReducer = (state = { counter: 0 }, action) => {
   switch (action.type) {
     case 'increment':
       return { counter: state.counter + 1 };
+    case 'increase':
+      return { counter: state.counter + action.amount };
     case 'decrement':
       return { counter: state.counter - 1 };
     default:
