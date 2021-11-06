ed5cfd78054ae67a1849d8f18f1c7e23e939197d
SOunit <workbookwork7@gmail.com>
2021-11-06 16:30:13 -0700

    add login

--- 00_practice/01-redux-function-reduce-toolkit/src/App.js
+++ 00_practice/01-redux-function-reduce-toolkit/src/App.js
@@ -1,9 +1,19 @@
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile';
 import Counter from './components/Counter';
-
+import Auth from './components/Auth';
+import Header from './components/Header';
 
 function App() {
+  const isLogin = useSelector((state) => state.auth.isLogin);
   return (
-    <Counter />
+    <Fragment>
+      {isLogin && <Header />}
+      {!isLogin && <Auth />}
+      {isLogin && <UserProfile />}
+      {isLogin && <Counter />}
+    </Fragment>
   );
 }
 
--- 00_practice/01-redux-function-reduce-toolkit/src/store/index.js
+++ 00_practice/01-redux-function-reduce-toolkit/src/store/index.js
@@ -1,6 +1,24 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 import counterReducer from './counter';
 
-const store = configureStore({ reducer: counterReducer });
+const authInitialState = { isLogin: false };
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: authInitialState,
+  reducers: {
+    login: (state) => {
+      state.isLogin = true;
+    },
+    logout: (state) => {
+      state.isLogin = false;
+    },
+  },
+});
+
+export const authActions = authSlice.actions;
+
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authSlice.reducer },
+});
 
 export default store;
