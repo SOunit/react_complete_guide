4fe7b731c67312ac909b0a70e4d76787dcd2eb5a
SOunit <workbookwork7@gmail.com>
2021-11-06 00:01:01 -0700

    add redux sample

--- 18_redux_intro/02_redux_react/src/store/index.js
+++ 18_redux_intro/02_redux_react/src/store/index.js
@@ -1,10 +1,11 @@
-import redux from 'redux';
 import { createStore } from 'redux';
 
 const counterReducer = (state = { counter: 0 }, action) => {
   switch (action.type) {
     case 'increment':
       return { counter: state.counter + 1 };
+    case 'decrement':
+      return { counter: state.counter - 1 };
     default:
       return state;
   }
