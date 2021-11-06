83ddb795a0c70c5a3d7037d64ef2cd11e4075e55
SOunit <workbookwork7@gmail.com>
2021-11-06 16:32:59 -0700

    refactor, separate files

--- 00_practice/01-redux-function-reduce-toolkit/src/store/index.js
+++ 00_practice/01-redux-function-reduce-toolkit/src/store/index.js
@@ -1,24 +1,9 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 import counterReducer from './counter';
-
-const authInitialState = { isLogin: false };
-const authSlice = createSlice({
-  name: 'auth',
-  initialState: authInitialState,
-  reducers: {
-    login: (state) => {
-      state.isLogin = true;
-    },
-    logout: (state) => {
-      state.isLogin = false;
-    },
-  },
-});
-
-export const authActions = authSlice.actions;
+import authReducer from './auth';
 
 const store = configureStore({
-  reducer: { counter: counterReducer, auth: authSlice.reducer },
+  reducer: { counter: counterReducer, auth: authReducer },
 });
 
 export default store;
