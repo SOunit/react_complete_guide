c425aa7a6689031306743a24825ca95351ad08ae
SOunit <workbookwork7@gmail.com>
2021-11-06 14:25:51 -0700

    add logout

--- 00_practice/01-redux-class/src/components/Header.js
+++ 00_practice/01-redux-class/src/components/Header.js
@@ -1,24 +1,39 @@
+import { Component } from 'react';
+import { LOGOUT } from '../store';
+import { connect } from 'react-redux';
 import classes from './Header.module.css';
 
-const Header = () => {
-  return (
-    <header className={classes.header}>
-      <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+class Header extends Component {
+  logoutHandler() {
+    this.props.logout();
+  }
+
+  render() {
+    return (
+      <header className={classes.header}>
+        <h1>Redux Auth</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href='/'>My Products</a>
+            </li>
+            <li>
+              <a href='/'>My Sales</a>
+            </li>
+            <li>
+              <button onClick={this.logoutHandler.bind(this)}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logout: () => dispatch({ type: LOGOUT }),
+  };
 };
 
-export default Header;
+export default connect(null, mapDispatchToProps)(Header);
