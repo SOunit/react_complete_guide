031cd9e4a47a86874f0e47109317d11234c1c9d6
SOunit <workbookwork7@gmail.com>
2021-11-06 10:16:24 -0700

    add practice

--- 00_practice/01-react-start copy/src/components/Counter.js
+++ 00_practice/01-react-start copy/src/components/Counter.js
@@ -1,15 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { counterActions } from '../store';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   const toggleCounterHandler = () => {};
-  const incrementHandler = () => {};
-  const increaseHandler = () => {};
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(10));
+  };
   const decrementHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      {<div className={classes.value}>{'counter'}</div>}
+      {<div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
---
+++ 00_practice/01-react-start copy/src/store/index.js
@@ -0,0 +1,21 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const initialState = { counter: 0, show: true };
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.counter++;
+    },
+    increase: (state, action) => {
+      state.counter += action.payload;
+    },
+  },
+});
+
+const store = configureStore({ reducer: counterSlice.reducer });
+
+export const counterActions = counterSlice.actions;
+export default store;
