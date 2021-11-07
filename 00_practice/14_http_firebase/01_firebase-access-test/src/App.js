946375daccd25d504ee7b1a158da8bd4020d06fa
SOunit <workbookwork7@gmail.com>
2021-11-07 15:33:30 -0800

    add firebase sample

--- 00_practice/14_http_firebase/01_firebase-access-test/src/App.js
+++ 00_practice/14_http_firebase/01_firebase-access-test/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [people, setPeople] = useState([]);
+
+  const fetchHandler = useCallback(async () => {
+    const res = await axios.get(
+      'https://fir-db-connection-sample-default-rtdb.firebaseio.com/people.json'
+    );
+
+    const people = [];
+    for (let key in res.data) {
+      const person = res.data[key];
+      console.log(person);
+      people.push(person);
+    }
+
+    setPeople(people);
+  }, []);
+
+  const saveHandler = async () => {
+    const res = await axios.post(
+      'https://fir-db-connection-sample-default-rtdb.firebaseio.com/people.json',
+      { id: Math.random(), name: 'Jack' }
+    );
+    console.log(res);
+  };
+
+  useEffect(() => {
+    fetchHandler();
+  }, [fetchHandler]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        {people.map((person) => (
+          <div key={person.id}>{person.name}</div>
+        ))}
+      </div>
+      <div>
+        <button onClick={fetchHandler}>Fetch</button>
+      </div>
+      <div>
+        <button onClick={saveHandler}>Save</button>
+      </div>
     </div>
   );
 }
