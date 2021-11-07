c6f4fa4aa947130ef17c9e23c7209ea746424e1e
SOunit <workbookwork7@gmail.com>
2021-11-07 12:24:17 -0800

    add adding item to cart function

--- 19_redux_advanced/01_sample_app/src/store/index.js
+++ 19_redux_advanced/01_sample_app/src/store/index.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import uiReducer from './ui-slice';
+import cartReducer from './cart-slice';
 
-const store = configureStore({ reducer: { ui: uiReducer } });
+const store = configureStore({ reducer: { ui: uiReducer, cart: cartReducer } });
 
 export default store;
