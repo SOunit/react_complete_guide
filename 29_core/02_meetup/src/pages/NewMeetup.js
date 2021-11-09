93a613c3277f1e3ca469c556f73ebf368dbec5dd
SOunit <workbookwork7@gmail.com>
2021-11-08 18:21:51 -0800

    add http request

--- 29_core/02_meetup/src/pages/NewMeetup.js
+++ 29_core/02_meetup/src/pages/NewMeetup.js
@@ -1,10 +1,26 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
+import axios from 'axios';
 
 const NewMeetupPage = () => {
+  const addMeetupHandler = async (meetupData) => {
+    try {
+      const res = await axios.post(
+        'https://fir-db-connection-sample-default-rtdb.firebaseio.com/meetups.json',
+        meetupData
+      );
+
+      if (res.status !== 200) {
+        throw new Error('Failed to post new meetup data');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
